8fe98d7cb7e48af0e66629855f66f24f4a5fa32c
Triumph <archie200@icloud.com>
2019-06-18 11:38:18 -0500

    added nasa api search

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -5,7 +5,7 @@ $( document ).ready(function() {
     console.log( "ready!" );
     $('select').formSelect();
     $('input#input_text, textarea#textarea2').characterCounter();
-});
+
 
 // VARIABLES
 // ===============================================================
@@ -15,8 +15,35 @@ $( document ).ready(function() {
 
 // FUNCTIONS
 // ===============================================================
+// Nasa API connection
+            function displayPlanetImage() {
+                var destination = $(this).attr("data-name");
+                // console.log(destination)
+
+                var queryURL = "https://images-api.nasa.gov/search?q=" + "destination";
+
+                // Ip2unDZzier4N1q7RpLlfMSHLWLoYDimT5hsxIzc
+                // console.log(queryURL)
+
+                $.ajax({
+                    url: queryURL,
+                    method: "GET"
+                }).then(function (response) {
+                    // console.log("planet requested")
+                    // console.log(response)
+                    // console.log(response.collection)
+                    // console.log(response.collection.href)
 
 
+                    // for (var i = 0; i < response.data.length; i++)
+
+                    $("body").append("<img src=" + response.collection.items[0].links[0].href + ">")
+                    console.log(response.collection.items[0].links[0].href);
+
+                });
+
+            }
+displayPlanetImage();
 
 
 
@@ -25,3 +52,5 @@ $( document ).ready(function() {
 
 // $('.dropdown-trigger').dropdown();
 // var planets = [mercury, venus, mars, jupiter, saturn, uranus, neptune, pluto];
+
+});
